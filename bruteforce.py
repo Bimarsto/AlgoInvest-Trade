97ced3e7ac8b3227f14248ea2d71e6e261f34768
Bimarsto <thibaudmars@hotmail.fr>
2022-10-21 16:35:18 +0200

    Factorisation de la solution de force brute.

--- bruteforce.py
+++ bruteforce.py
@@ -1,53 +1,62 @@
 # Modules & Packages
 import csv
-import itertools
+from itertools import combinations
 import time
 
 # Variables
-data = []
 max_cost = 500
-best_combination = []
-active_cost = 0
-active_profit = 0
-best_profit = 0
 
-# choisir le fichier csv
-# importer les données dans un tableau
+
+def get_data_from_csv(path):
+    data = []
+    with open(path, 'r') as csvfile:
+        file = csv.reader(csvfile)
+        for row in file:
+            try:
+                row[1] = float(row[1])
+                row[2] = float(row[2])
+                if float(row[1]) > 0:
+                    data.append(row)
+            except:
+                pass
+    return data
+
+
+def find_all_combinations(data, max_cost):
+    all_combinations = []
+    for n in range(len(data) + 1):
+        for subset in combinations(data, n):
+            active_combination_cost = 0
+            active_combination_profit = 0
+            for item in subset:
+                active_combination_cost += item[1]
+                active_combination_profit += (item[2] / 100) * item[1]
+            if active_combination_cost <= max_cost:
+                all_combinations.append([subset, active_combination_cost, active_combination_profit])
+    return all_combinations
+
+
+def find_best_profit(data):
+    best_combination_profit = 0
+    best_combination = []
+    for combination in data:
+        if combination[2] > best_combination_profit:
+            best_combination_profit = combination[2]
+            best_combination = combination
+    return best_combination
+
+
+def display_result(result):
+    for item in best_combination[0]:
+        print(item[0])
+    print(f"Coût total des actions : {best_combination[1]}€")
+    print(f"Gain sur 2 ans : {best_combination[2]}€")
+    print(f"Fini en {end-start} sec")
+
+
+raw_data = get_data_from_csv('./data/actions.csv')
 start = time.time()
-with open('./data/actions.csv', 'r') as csvfile:
-    file = csv.reader(csvfile)
-    for row in file:
-        try:
-            row[1] = float(row[1])
-            row[2] = float(row[2])
-        except:
-            pass
-
-        data.append(row)
-    data.pop(0)
-
-# traiter les données
-
-# algo
-    # Trouver toutes les combinaisons
-for n in range(0, len(data)-1):
-    for subset in itertools.combinations(data, n):
-        active_cost = 0
-        active_profit = 0
-        for item in subset:
-            active_cost += item[1]
-            active_profit += (item[2] / 100) * item[1]
-        if 0 < active_cost <= max_cost and active_profit > best_profit:
-            best_profit = active_profit
-            best_combination = subset
-
-# affichage des résultats
+all_combinations = find_all_combinations(raw_data, max_cost)
+best_combination = find_best_profit(all_combinations)
 end = time.time()
-active_cost = 0
-for item in best_combination:
-    print(item[0])
-    active_cost += item[1]
-print(f"Coût total des actions : {active_cost}€")
-print(f"Gain sur 2 ans : {best_profit}€")
-print(f"Fini en {end-start} sec")
-
+display_result(best_combination)
